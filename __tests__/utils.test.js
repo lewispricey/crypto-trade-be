3ddba12064af03e8883f03d232ff62b24ee1c829
Lewis Price <lewis@Lewiss-MacBook-Pro-719.local>
2023-03-08 19:19:10 +0000

    adds util functions to check email and password inputs

---
+++ __tests__/utils.test.js
@@ -0,0 +1,57 @@
+const checkEmail = require("../utils/checkEmail");
+const checkPassword = require("../utils/checkPassword");
+
+describe("checkEmail", () => {
+  test("should return false when passed an email without an @ symbol", () => {
+    const input = "testemail.com";
+    const result = checkEmail(input);
+    expect(result).toBe(false);
+  });
+  test("should return false when passed an email without a domain", () => {
+    const input = "testemail1234";
+    const result = checkEmail(input);
+    expect(result).toBe(false);
+  });
+  test("should return true when passed a valid email", () => {
+    const input = "testemail@email.com";
+    const result = checkEmail(input);
+    expect(result).toBe(true);
+
+    const input2 = "thisisanemail@hotmail.co.uk";
+    const result2 = checkEmail(input2);
+    expect(result2).toBe(true);
+  });
+});
+
+describe("checkPassword", () => {
+  test("should return false when passed a password less than 8 charactors", () => {
+    const input = "L0gin!";
+    const result = checkPassword(input);
+    expect(result).toBe(false);
+  });
+  test("should return false when passed a password missing a numeric character", () => {
+    const input = "Password!";
+    const result = checkPassword(input);
+    expect(result).toBe(false);
+  });
+  test("should return false when passed a password missing a uppercase character", () => {
+    const input = "passw0rd!";
+    const result = checkPassword(input);
+    expect(result).toBe(false);
+  });
+  test("should return false when passed a password missing a special character", () => {
+    const input = "P4ssw0rd";
+    const result = checkPassword(input);
+    expect(result).toBe(false);
+  });
+  test("should return false when passed a password missing a lowercase character", () => {
+    const input = "P4SSWORD!";
+    const result = checkPassword(input);
+    expect(result).toBe(false);
+  });
+  test("should return true when passed a vaid password", () => {
+    const input = "P4ssW0RD!";
+    const result = checkPassword(input);
+    expect(result).toBe(true);
+  });
+});
---
+++ utils/checkEmail.js
@@ -0,0 +1,6 @@
+const checkEmail = (email) => {
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return emailRegex.test(email);
+};
+
+module.exports = checkEmail;
