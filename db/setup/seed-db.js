1b88e376ab75e2348c66cd79bafbfa104b6b3213
Lewis Price <lewis@Lewiss-MacBook-Pro-719.local>
2023-03-09 20:40:40 +0000

    adds a test user to the seed file for testing

--- db/setup/seed-db.js
+++ db/setup/seed-db.js
@@ -18,9 +18,29 @@ const setupTables = async () => {
   );`);
 };
 
+const addData = async () => {
+  const userResponse = await db.query(
+    `INSERT INTO users
+      (email, password) 
+      VALUES
+      ($1, $2) 
+      RETURNING*;`,
+    [
+      "test@lprice.dev",
+      "$argon2id$v=19$m=65536,t=12,p=4$xtuHAXS+877DIrv9Viob8Q$qZWhV5oI7skCki7kcK+ZcwSZ/46631p5etD0HN6Cngg",
+    ]
+  );
+  await db.query(
+    `INSERT INTO profile(user_id)
+      VALUES($1);`,
+    [userResponse.rows[0].user_id]
+  );
+};
+
 const seedDb = async () => {
   await dropTables();
   await setupTables();
+  await addData();
 };
 
 module.exports = seedDb;
