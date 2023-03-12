a021134ddf73bc9426d99ae8eb6c97d9e6dc0504
Lewis Price <1995ldp@gmail.com>
2023-03-12 21:37:48 +0000

    adds beforeAll login to get token

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,20 @@ const app = require("../app");
 const seedDb = require("../db/setup/seed-db");
 const db = require("../db/connection");
 
+let token = "";
+
+beforeAll(async () => {
+  await seedDb();
+  const postBody = {
+    email: "test@lprice.dev",
+    password: "isS1ANZ*#ESaRVIUgdnC9!$*",
+  };
+
+  const { body } = await request(app).post("/auth/login").send(postBody);
+
+  token = body.accessToken;
+});
+
 beforeEach(async () => {
   await seedDb();
 });
@@ -12,7 +26,9 @@ afterAll(() => db.end());
 describe("/status", () => {
   describe("GET", () => {
     test("200 - returns an object with the key of online", async () => {
-      const { status, body } = await request(app).get("/status");
+      const { status, body } = await request(app)
+        .get("/status")
+        .set({ Authorization: `Bearer ${token}` });
       expect(status).toBe(200);
       expect(body).toEqual({ msg: "online" });
     });
